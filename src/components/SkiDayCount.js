39ee7bd5ec804eaf41c8c6dea71a4368c4d8f99c
Chris Qiu <churchqq030@hotmail.com>
2018-06-10 18:23:31 +1000

    upto form refs

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -3,6 +3,7 @@ import '../stylesheets/ui.scss'
 import Terrain from 'react-icons/lib/md/terrain'
 import SnowFlake from 'react-icons/lib/ti/weather-snow'
 import Calendar from 'react-icons/lib/fa/calendar'
+import {PropTypes} from 'react'
 
 const percentToDecimal = (decimal) => {
     return ((decimal *100 ) + '%')
@@ -38,3 +39,17 @@ export const SkiDayCount = ( {total,powder,backcountry,goal} ) => (
         )
     
 
+SkiDayCount.defaultProps = {
+total: 50,
+powder: 10,
+backcountry: 15,
+goal:75
+}
+          
+
+SkiDayCount.propTypes = {
+    total: PropTypes.number,
+    powder: PropTypes.number,
+    backcountry: PropTypes.number,
+    goal:PropTypes.number
+  }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { SkiDayList} from './components/SkiDayList'
 import { SkiDayCount} from './components/SkiDayCount'
+import {App} from './components/App'
+import {Whoops404} from './components/Whoops404'
+import { Router, Route,hashHistory } from 'react-router'
 
 window.React = React
 
+// render(
+//     <SkiDayCount backcountry={false}/>,
+//     document.getElementById('react-container')
+// )
+
+
 render(
-    <SkiDayCount total={50}
-                 powder={20}
-                 backcountry={10}
-                 goal={100}/>,
+    <Router history={hashHistory}>
+        <Route path="/" component={App}/> 
+        <Route path="list-days" component={App}>
+            <Route path=":filter" component={App} />
+        </Route> 
+        <Route path="add-day" component={App}/>                 
+        <Route path="*" component={Whoops404}/>
+    </Router>,
     document.getElementById('react-container')
 )
